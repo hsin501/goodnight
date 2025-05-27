929eec231e4c62a20583cd824eb1e5c63f2b39bc
hsin <joanne45644@gmail.com>
2025-05-27 11:40:43 +0800

    重構試試

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-/**
- * 常量
- */
-export const OBJECT_DISTANCE = 5; // 物件之間的垂直距離
-export const INITIAL_MESH_COLOR = '#81D4FA'; // 模型初始顏色
-
-export const MOBILE_BREAKPOINT = 768; // 移動設備斷點寬度
-console.log('常量模塊 (constants.js) 已加載');
